3be0893e10ecca7e3196ddad33ef3a731c4e1126
kamszeliga <kamrynszeliga@gmail.com>
2023-03-24 16:10:41 -0400

    Empty Screens w/ Text WIP

--- Controllers/CompaniesController.cs
+++ Controllers/CompaniesController.cs
@@ -143,6 +143,7 @@ namespace MeteorStrike.Controllers
         }
 
         // GET: Companies/Create
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
             return View();
@@ -151,6 +152,7 @@ namespace MeteorStrike.Controllers
         // POST: Companies/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Description,ImageFileData,ImageFileType")] Company company)
